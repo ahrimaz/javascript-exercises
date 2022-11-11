9c1cd6d2553ac7ddea9acb191ce0b7ee0a0ed040
ahrimaz <eric.stanard@gmail.com>
2022-11-10 21:27:33 -0500

    completed 02_repeatString - all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function stringRepeater(string, times) {
+    var repeatedString = "";
+        if (times >= 0) {
+            while (times > 0) {
+                repeatedString += string;
+                times --;
+            }
+            return repeatedString;
+        } else {
+            let repeatedString = "ERROR";
+            return repeatedString;
+        }
+}
 
-};
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("", 5);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
